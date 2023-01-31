df04bd1555246830fe88c0c52251e24729f05a8a
Thongkorn Tubtimkrob <thongkorn@hotmail.com>
2023-01-31 15:52:34 +0700

    Add files via upload

---
+++ AutoCompleteSyncfusion/AutoCompleteSyncfusion/frmAutoComplete.Designer.vb
@@ -0,0 +1,103 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmAutoComplete
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.AutoComplete1 = New Syncfusion.Windows.Forms.Tools.AutoComplete(Me.components)
+        CType(Me.AutoComplete1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(73, 48)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(243, 26)
+        Me.TextBox1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(70, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(171, 18)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "AutoComplete DataTable"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(73, 111)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(243, 26)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(70, 90)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(167, 18)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "AutoComplete DataBase"
+        '
+        'AutoComplete1
+        '
+        Me.AutoComplete1.HeaderFont = New System.Drawing.Font("Segoe UI", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World)
+        Me.AutoComplete1.ItemFont = New System.Drawing.Font("Segoe UI", 8.25!)
+        Me.AutoComplete1.MetroColor = System.Drawing.Color.FromArgb(CType(CType(17, Byte), Integer), CType(CType(158, Byte), Integer), CType(CType(218, Byte), Integer))
+        Me.AutoComplete1.ParentForm = Me
+        Me.AutoComplete1.Style = Syncfusion.Windows.Forms.Tools.AutoCompleteStyle.[Default]
+        Me.AutoComplete1.ThemeName = "Default"
+        '
+        'frmAutoComplete
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(393, 205)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Font = New System.Drawing.Font("Tahoma", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(222, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "frmAutoComplete"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "AutoComplete Syncfusion"
+        CType(Me.AutoComplete1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents AutoComplete1 As Syncfusion.Windows.Forms.Tools.AutoComplete
+
+End Class
---
+++ AutoCompleteSyncfusion/AutoCompleteSyncfusion/frmAutoComplete.vb
@@ -0,0 +1,131 @@
+﻿#Region "ABOUT"
+' / --------------------------------------------------------------------------------
+' / Developer : Mr.Surapon Yodsanga (Thongkorn Tubtimkrob)
+' / eMail : thongkorn@hotmail.com
+' / URL: http://www.g2gnet.com (Khon Kaen - Thailand)
+' / Facebook: https://www.facebook.com/g2gnet (For Thailand)
+' / Facebook: https://www.facebook.com/commonindy (Worldwide)
+' / More Info: http://www.g2gsoft.com/
+' /
+' / Purpose: AutoComplete in TextBox Control with Syncfusion.
+' / Microsoft Visual Basic .NET (2010)
+' /
+' / This is open source code under @Copyleft by Thongkorn Tubtimkrob.
+' / You can modify and/or distribute without to inform the developer.
+' / --------------------------------------------------------------------------------
+#End Region
+
+Imports System.Data.OleDb
+Imports Syncfusion.Windows.Forms.Tools
+
+Public Class frmAutoComplete
+
+    Private Conn As New OleDbConnection
+    Dim autoComplete As AutoComplete = New AutoComplete
+
+    ' / --------------------------------------------------------------------------------
+    Private Sub frmAutoComplete_Load(sender As System.Object, e As System.EventArgs) Handles MyBase.Load
+        ' / --------------------------------------------------------------------------------
+        '// AutoComplete with sample DataTable.
+        AutoComplete1.SetAutoComplete(TextBox1, AutoCompleteModes.Both) '// Mode Append + Suggest.
+        Me.AutoComplete1.DataSource = MyDataTable()
+
+        '// AutoComplete with DataTable from DataBase and create it with code.
+        Call ConnectDB()
+        With autoComplete
+            .ParentForm = Me    '// Must have for AutoComplete @Run time.
+            .ShowColumnHeader = True
+            .ShowCloseButton = True
+            .ShowGripper = True
+            '.AdjustHeightToItemCount = False
+            '.AutoPersistentDropDownSize = True
+            '.Style = AutoCompleteStyle.Metro
+        End With
+        autoComplete.SetAutoComplete(TextBox2, AutoCompleteModes.MultiSuggestExtended)
+        autoComplete.DataSource = GetDataTable()
+    End Sub
+
+    ' / --------------------------------------------------------------------------------
+    '// Create Sample DataTable.
+    ' / --------------------------------------------------------------------------------
+    Function MyDataTable() As DataTable
+        '// Create DataTable
+        Dim dt As DataTable = New DataTable()
+        '// Add Columns
+        With dt.Columns
+            .Add("Country")
+            .Add("Capital")
+        End With
+        '// Add Rows
+        With dt.Rows
+            .Add(New String() {"Thailand", "Bangkok"})
+            .Add(New String() {"United Kingdom", "London"})
+            .Add(New String() {"USA", "Washington, D.C."})
+            .Add(New String() {"Brazil", "Brasilia"})
+            .Add(New String() {"France", "Paris"})
+            .Add(New String() {"Russia", "Moscow"})
+            .Add(New String() {"India", "Delhi"})
+            .Add(New String() {"Japan", "Tokyo"})
+            .Add(New String() {"Taiwan", "Taipei"})
+            .Add(New String() {"Croatia", "Zagreb"})
+            .Add(New String() {"Brunei", "Bandar Seri Begawan"})
+        End With
+        '// Return DataTable
+        Return dt
+    End Function
+
+    ' / --------------------------------------------------------------------------------
+    '// DataTable from DataBase.
+    ' / --------------------------------------------------------------------------------
+    Function GetDataTable() As DataTable
+        Dim dt As New DataTable
+        Dim strSQL As String = String.Empty
+        '//
+        strSQL = _
+            " SELECT Country, Capital " & _
+            " FROM(Table1) " & _
+            " ORDER BY Country, Capital "
+        If Conn.State = ConnectionState.Closed Then Conn.Open()
+        Using DA As New OleDbDataAdapter(strSQL, Conn)
+            DA.Fill(dt)
+        End Using
+        '// Return DataTable
+        Return dt
+    End Function
+
+    Public Function ConnectDB() As Boolean
+        Dim strConn As String = _
+            " Provider = Microsoft.ACE.OLEDB.12.0; " & _
+            " Data Source = " & MyPath(Application.StartupPath) & "data\SampleDB.accdb"
+        Try
+            Conn = New OleDb.OleDbConnection(strConn)
+            Conn.Open()
+            '// Return
+            Return True
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+            Conn = Nothing
+            Return False
+        End Try
+    End Function
+
+    ' / --------------------------------------------------------------------------------
+    ' / Get my project path
+    ' / AppPath = C:\My Project\bin\debug
+    ' / Replace "\bin\debug" with "\"
+    ' / Return : C:\My Project\
+    Function MyPath(ByVal AppPath As String) As String
+        '/ MessageBox.Show(AppPath);
+        AppPath = AppPath.ToLower()
+        '/ Return Value
+        MyPath = AppPath.Replace("\bin\debug", "\").Replace("\bin\release", "\").Replace("\bin\x86\debug", "\").Replace("\bin\x86\release", "\")
+        '// If not found folder then put the \ (BackSlash - ASCII Code = 92) at the end.
+        If Microsoft.VisualBasic.Right(MyPath, 1) <> Chr(92) Then MyPath = MyPath & Chr(92)
+    End Function
+
+    Private Sub frmAutoComplete_FormClosed(sender As Object, e As System.Windows.Forms.FormClosedEventArgs) Handles Me.FormClosed
+        Me.Dispose()
+        GC.SuppressFinalize(Me)
+        End
+    End Sub
+End Class
